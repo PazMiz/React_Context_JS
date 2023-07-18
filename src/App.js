3327a8d7ab857ee25a6c999272fd4e69d85a3d93
PazMiz <127669477+PazMiz@users.noreply.github.com>
2023-07-18 09:31:43 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from 'react';
+import { CounterProvider } from './Countercontext/CounterContext';
+import { ColorProvider } from './ColorContext/ColorContext';
+import CounterDisplay from './Countercontext/CounterDisplay';
+import CounterButtons from './Countercontext/CounterButtons';
+import CounterInfo from './Countercontext/CounterInfo';
+import ColorPicker from './ColorContext/ColorPicker';
+import ColoredBox from './ColorContext/ColoredBox'; // Import the new component
+import { useTheme } from './ThemeContext';
+
+const App = () => {
+  const { theme, toggleTheme, resetTheme } = useTheme(); // Use the theme, toggleTheme, and resetTheme from the ThemeContext
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorProvider>
+      <CounterProvider>
+        <div style={{ backgroundColor: theme, padding: '20px' }}>
+          <h1>React Context Counter App</h1>
+          <button onClick={toggleTheme}>Toggle Theme</button> {/* Add a button to toggle the theme */}
+          <button onClick={resetTheme}>Reset Theme</button> {/* Add a button to reset the theme */}
+          <CounterDisplay />
+          <CounterButtons />
+          <CounterInfo />
+          <ColorPicker />
+          <ColoredBox /> {/* Add the ColoredBox component here */}
+        </div>
+      </CounterProvider>
+    </ColorProvider>
   );
-}
+};
 
 export default App;
